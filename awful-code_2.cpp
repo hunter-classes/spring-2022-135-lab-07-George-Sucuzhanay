746094d89ee4833498bf84ec828cb900c54c020b
George <georgesucuzhanay100@gmail.com>
2022-04-14 09:05:01 -0400

    added a new func to style awful-code-2.cpp

---
+++ awful-code_2.cpp
@@ -0,0 +1,13 @@
+     #include <iostream>
+     int main(){
+int myInt = 1; 
+    
+for(int i = 0; i < 10; i++)
+
+
+
+ {
+                        std::cout << i;
+    std::cout << std::endl;
+ }
+    }
