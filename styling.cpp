9a90f1e69685dc31db25611e55fe3039dcb793ca
Alexander-col <aexro2221@gmail.com>
2022-04-27 15:28:45 -0400

    added a new unident function

--- styling.cpp
+++ styling.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include "styling.h"
 
+std::string removeLeadingSpaces(std::string str)
+{
+    std::string output = "";
+    int sl = str.length();
+    int ns = 0; // ns stands for No Space
+    for (int i = 0; i != sl; i++)
+    {
+        if (str[i] != ' ')
+        {
+        ns = i;
+        break;
+        }
+    }
 
+    for (int j = ns; j != sl; j++)
+    {
+        output += str[j];
+    }
+
+    return output;
+
+
+} 
 
 int countChar(std::string line, char c)
 {
