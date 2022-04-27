0cbf4f047306751970941506b7ea7ed4cb9b57ca
Alexander-col <aexro2221@gmail.com>
2022-04-27 15:25:49 -0400

    removed the unindent func from styling.cpp -Alexander

--- styling.cpp
+++ styling.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
 #include "styling.h"
 
-std::string removeLeadingSpaces(std::string line)
-{
-    std::string result;
-    
-    int i = 0;
-    if(isspace(line[i]))
-    {
-        while (isspace(line[i]))
-        {
-            i++;
-        }
-    }
-    
-    result = line.substr(i);
-    return result;
-}
+
 
 int countChar(std::string line, char c)
 {
